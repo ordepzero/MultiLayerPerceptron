813e91ed6e53726653f0e4936f3ffee2399b2564
Ulises PH <pedro.ul@hotmail.com>
2016-09-06 19:46:30 -0300

    Second layer entries

--- MLP/main.py
+++ MLP/main.py
@@ -8,6 +8,8 @@ import math
 import numpy as np
 
 
+
+
 def tangente_hyperbolique(a):
     
     return (math.tanh(a))
@@ -19,15 +21,22 @@ def multiply_matrix(m, c):
     #print(r)
     return (np.sum(r,axis=1).tolist())
     
-
+def activation_function(values):
+   values = [ tangente_hyperbolique(v) for v in values]
+   return values
+    
 if __name__ == "__main__":
     
     number_of_neurons = 3
     number_of_entries = 3
     entries = [-1, 0.3, 0.7]
     
-    firts_layer_weights = [[0 for col in range(number_of_entries)] for row in range(number_of_neurons)]
+    #firts_layer_weights = [[0 for col in range(number_of_entries)] for row in range(number_of_neurons)]
+    firts_layer_weights = [[0.2, 0.4, 0.5],[0.3, 0.6, 0.7], [0.4, 0.8, 0.3]]
     
-    print (multiply_matrix(firts_layer_weights, entries))
+    second_layer_entries = [-1] + activation_function(multiply_matrix(firts_layer_weights, entries))
+
+    print(second_layer_entries)
+    
     
     
